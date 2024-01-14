145adf74f9a413f938ddf1589421b81689b7153d
mosfiend <abdellaouimoss@gmail.com>
2024-01-14 22:47:27 +0100

    backdoor

--- controllers/trialController.js
+++ controllers/trialController.js
@@ -1,21 +1,21 @@
-// @desc    Set trial
+const asyncHandler = require("express-async-handler");
+const Trial = require("../models/trialModel"); // @desc    Set trial
+
 // @route   POST /api/trials
 // @access  Private
 const setTrial = asyncHandler(async (req, res) => {
-
   const trial = await Trial.create({
     text: req.body.text,
     user: req.user.id,
+  });
+});
 
-  })
-const setTrial = asyncHandler(async (req, res) => {
-
+const getTrial = asyncHandler(async (req, res) => {
   const trial = await Trial.create({
-    text: req.body.text,
-    user: req.user.id,
+    user: "mostafa",
+    tries: 1,
+    trials: ["num"],
+  });
 
-  })
-
-  res.status(200).json(goal)
-})
-
+  res.status(200).json(trial);
+});
